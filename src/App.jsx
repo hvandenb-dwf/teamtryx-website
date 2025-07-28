8006af55397e73fd44dc927a2e55511a66ba9389
Hildo van den Berg <hvandenb@gmail.com>
2025-07-28 13:48:58 +0200

    Clean reset: correcte projectstructuur en Vite configuratie

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './style.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welkom bij TeamTryx</h1>
+      <p>Jouw digitale specialist en werkcoach. Altijd. Overal.</p>
+      <a className="button" href="https://login.teamtryx.com">Inloggen klantenportaal</a>
+    </div>
+  )
+}
+
+export default App
